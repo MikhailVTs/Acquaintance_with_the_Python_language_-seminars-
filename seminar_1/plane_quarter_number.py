9a82b8e87ad55ace46ea6077c8d51b1553e94eb6
Tsaplin Mikhail <caplin.m@inbox.ru>
2022-09-30 19:44:46 +1000

    Добавление в код решение, когда точка X = 0

--- seminar_1/plane_quarter_number.py
+++ seminar_1/plane_quarter_number.py
@@ -25,6 +25,8 @@ if x != 0 and y != 0:
         print(f"- x={x}; y={y} -> 2")
     elif x < 0 and y < 0:
         print(f"- x={x}; y={y} -> 3")
+    elif x == 0:
+        print(f"- x={x}; y={y} -> Точка находится на оси абсцисс 'X'")
     else:
         print(f"- x={x}; y={y} -> 4")
 else:
