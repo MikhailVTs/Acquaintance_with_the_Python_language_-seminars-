eff523eb00057767e64950159966da75c020e1dc
Mikhail <caplin.m@inbox.ru>
2022-11-12 14:42:27 +1000

    Homework_for_workshop_9

---
+++ Seminar_9/tic_tac_toe/tic_tac_toe.py
@@ -0,0 +1,103 @@
+from tkinter import *
+from tkinter import messagebox
+
+
+count=0
+board=[['','','',],
+           ['','','',],
+           ['','','',]]
+
+def Quit():
+    global t    
+    msg=messagebox.askquestion("Подтвердить", "Вы хотите выйти? У вас еще есть шансы!")
+    if msg=='yes':
+        t.destroy()
+
+
+def destruct():
+    global t,winnerWindow
+    t.destroy()
+    winnerWindow.destroy()
+
+
+def displayWinner(winner):
+    global t,winnerWindow,ID    
+    winnerWindow=Tk()
+    winnerWindow.title("Окно победителя")
+    winnerWindow.configure(bg="Black")
+    l1=Label(winnerWindow,text="ПОБЕДИТЕЛЬ: ",font=("COMIC SANS MS",15),bg="Black",fg="White")
+    l1.pack()
+    l2=Label(winnerWindow,text=winner,font=("COMIC SANS MS",15),bg="Black",fg="White")
+    l2.pack()
+    bproceed=Button(winnerWindow,text="Продолжить",font=("COMIC SANS MS",10,"bold"),command=destruct)
+    bproceed.pack()
+
+       
+def checkWinner():
+    global count,board
+    if (board[0][0]==board[0][1]==board[0][2]=="X" or board[1][0]==board[1][1]==board[1][2]=="X" or board[2][0]==board[2][1]==board[2][2]=="X" or
+        board[0][0]==board[1][0]==board[2][0]=="X" or board[0][1]==board[1][1]==board[2][1]=="X" or board[0][2]==board[1][2]==board[2][2]=="X" or
+        board[0][0]==board[1][1]==board[2][2]=="X" or board[0][2]==board[1][1]==board[2][0]=="X"):
+            displayWinner("Игрок X")
+    elif (board[0][0]==board[0][1]==board[0][2]=="O" or board[1][0]==board[1][1]==board[1][2]=="O" or board[2][0]==board[2][1]==board[2][2]=="O" or
+          board[0][0]==board[1][0]==board[2][0]=="O" or board[0][1]==board[1][1]==board[2][1]=="O" or board[0][2]==board[1][2]==board[2][2]=="O" or
+          board[0][0]==board[1][1]==board[2][2]=="O" or board[0][2]==board[1][1]==board[2][0]=="O"):
+            displayWinner("Игрок O")
+    elif count==9:
+        displayWinner("Никто! Это НИЧЬЯ!")
+
+
+def changeVal(button,boardValRow,boardValCol):
+    global count
+
+    if button["text"]=="":
+        if count%2==0:
+            button["text"]="X"
+            l1=Label(t,text="PLAYER: 2(O)",height=3,font=("COMIC SANS MS",10,"bold"),bg="white").grid(row=0,column=0)
+            board[boardValRow][boardValCol]="X"
+        else:
+            button["text"]="O"
+            l1=Label(t,text="PLAYER: 1(X)",height=3,font=("COMIC SANS MS",10,"bold"),bg="white").grid(row=0,column=0)
+            board[boardValRow][boardValCol]="O"
+        count=count+1
+        if count>=5:
+            checkWinner()
+    else:
+        messagebox.showerror("Ошибка","Это поле уже занято!")
+
+
+def TicTacToeGUI():
+    global t
+    t=Tk()
+    t.title("Крестики Нолики")
+    t.configure(bg="white")  
+   
+    l1=Label(t,text="PLAYER: 1(X)",height=3,font=("COMIC SANS MS",10,"bold"),bg="white")
+    l1.grid(row=0,column=0)
+   
+    exitButton=Button(t,text="Quit",command=Quit,font=("COMIC SANS MS",10,"bold"))
+    exitButton.grid(row=0,column=2)  
+    
+    b1=Button(t,text="",height=4,width=8,bg="black",activebackground="white",fg="white",font="Times 15 bold",command=lambda: changeVal(b1,0,0))
+    b2=Button(t,text="",height=4,width=8,bg="black",activebackground="white",fg="white",font="Times 15 bold",command=lambda: changeVal(b2,0,1))
+    b3=Button(t,text="",height=4,width=8,bg="black",activebackground="white",fg="white",font="Times 15 bold",command=lambda: changeVal(b3,0,2))
+    b4=Button(t,text="",height=4,width=8,bg="black",activebackground="white",fg="white",font="Times 15 bold",command=lambda: changeVal(b4,1,0))
+    b5=Button(t,text="",height=4,width=8,bg="black",activebackground="white",fg="white",font="Times 15 bold",command=lambda: changeVal(b5,1,1))
+    b6=Button(t,text="",height=4,width=8,bg="black",activebackground="white",fg="white",font="Times 15 bold",command=lambda: changeVal(b6,1,2))
+    b7=Button(t,text="",height=4,width=8,bg="black",activebackground="white",fg="white",font="Times 15 bold",command=lambda: changeVal(b7,2,0))
+    b8=Button(t,text="",height=4,width=8,bg="black",activebackground="white",fg="white",font="Times 15 bold",command=lambda: changeVal(b8,2,1))
+    b9=Button(t,text="",height=4,width=8,bg="black",activebackground="white",fg="white",font="Times 15 bold",command=lambda: changeVal(b9,2,2))
+
+    b1.grid(row=2,column=0)
+    b2.grid(row=2,column=1)
+    b3.grid(row=2,column=2)
+
+    b4.grid(row=3,column=0)
+    b5.grid(row=3,column=1)
+    b6.grid(row=3,column=2)
+
+    b7.grid(row=4,column=0)
+    b8.grid(row=4,column=1)
+    b9.grid(row=4,column=2)
+
+TicTacToeGUI()
